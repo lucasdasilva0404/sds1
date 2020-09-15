139fd371ce84faedcce0b5ef7bfd134baa272fa1
Lucas A <lu-civic@hotmail.com>
2020-09-15 17:48:50 -0300

    Project Created

---
+++ backend/src/test/java/com/lucas/despesquisa/DspesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.despesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
